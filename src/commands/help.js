dbb419bdca3f37d144db127ed4165c6acc65185e
SrWhale <52763403+SrWhale@users.noreply.github.com>
2020-08-23 17:55:08 -0300

    Create help.js

---
+++ src/commands/help.js
@@ -0,0 +1,21 @@
+const { Command } = require('../index.js');
+
+module.exports = class HelpCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'help',
+            description: 'Veja os comandos do BOT',
+            aliases: ['ajuda']
+        })
+    }
+
+    async run() {
+        const prefix = await this.client.getPrefix(this.message.guild.id);
+
+        this.send(new this.client.embed()
+            .setAuthor("Pterodactyl Panel - Ajuda", this.client.user.displayAvatarURL())
+            .setDescription(`${this.client.commands.map(command => ` \`${prefix}${command.help.name}\` - ${command.help.description} `).join("\n")}`)
+            .setFooter(`Pterodactyl Panel - Ajuda`, this.client.user.displayAvatarURL()));
+
+    }
+}
